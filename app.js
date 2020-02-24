93e1c8701c4196794cebb8e0a2dd7ae5050d6d1e
veecee <valentine424@yahoo.com>
2020-02-24 13:01:23 +0100

    color changes and minor redsign

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let app = express();
 app.use(express.static("dist"));
 
 app.get("/", function(req, res) {
-    res.sendFile("../dist/index.html");
+ res.sendFile("../dist/index.html");
 });
 
 app.listen(process.env.PORT || 3000, function() {
